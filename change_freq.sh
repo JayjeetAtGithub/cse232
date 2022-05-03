549da6f50d11f1f26aa56da4e6966e729f8ed486
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-05-03 14:09:23 -0700

    Fix change_freq.sh

--- change_freq.sh
+++ change_freq.sh
@@ -3,13 +3,13 @@ set -ex
 
 freq=$1
 
-for $n in {1..8}
+for n in {1..8}
 do
     ssh node$n apt update
     ssh node$n apt install -y indicator-cpufreq
     ssh node$n apt install -y cpufrequtils
 
-    for $i in {0..7}
+    for i in {0..7}
     do 
         ssh node$n cpufreq-set -c $i -g performance -f $freq
     done
