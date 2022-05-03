719d0879e03f1ececcb7bbdf5bb5557eac735f9e
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-05-03 14:41:59 -0700

    Fix change_freq.sh

--- change_freq.sh
+++ change_freq.sh
@@ -9,7 +9,7 @@ do
     ssh node$n apt install -y indicator-cpufreq
     ssh node$n apt install -y cpufrequtils
 
-    for i in {0..7}
+    for i in {0..15}
     do 
         ssh node$n sudo cpufreq-set -c $i -f $freq
     done
