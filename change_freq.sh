05bc979f20c50560540dcb7e941d5ec076359105
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-05-03 08:37:01 -0700

    Create CPU freq set script

---
+++ change_freq.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -ex
+
+freq=$1
+
+for $n in {1..8}
+do
+    ssh node$n apt update
+    ssh node$n apt install -y indicator-cpufreq
+    ssh node$n apt install -y cpufrequtils
+
+    for $i in {0..7}
+    do 
+        ssh node$n cpufreq-set -c $i -g performance -f $freq
+    done
+done
+    
