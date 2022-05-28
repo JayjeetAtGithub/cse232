81d3ae678751f85391e6348ebc561502db03e069
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-05-28 12:58:01 -0700

    add network bottlenecking script

--- network.sh
+++ network.sh
@@ -7,12 +7,12 @@ IFS=',' read -r -a node_list <<< "$node_list"
 apt update
 apt install -y wondershaper
 sudo wondershaper clear eno1d1
-sudo wondershaper eno1d1 2048000 2048000
+sudo wondershaper eno1d1 5120000 5120000
 
 for node in ${node_list[@]}
 do	
     ssh $node "apt update"
     ssh $node "apt install -y wondershaper"
-    ssh $node "sudo wondershaper -c eno1d1"
-    ssh $node "sudo wondershaper -a eno1d1 -u 5120000 -d 5120000"
+    ssh $node "sudo wondershaper clear eno1d1"
+    ssh $node "sudo wondershaper eno1d1 5120000 5120000"
 done
